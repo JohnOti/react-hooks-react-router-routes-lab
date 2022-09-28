3718606fdec0bf565072d23a1a3a65037f892f44
John Oduor Otieno <johnoduor222@gmail.com>
2022-09-28 05:06:42 -0700

    Completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { directors } from "../data";
+// import Movies from "./Movies";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const director=directors.map((director)=> {
+    const movies=director.movies
+
+    return (
+      <div key={director.name}>
+        <h2>{director.name}</h2>
+        <ul>
+          {movies.map((movie,index)=>(
+            <li key={movie}>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    )
+
+  })
+
+
+    return (
+    <div>
+      <h1>Directors Page</h1>
+      {director}
+    </div>);
 }
 
 export default Directors;
